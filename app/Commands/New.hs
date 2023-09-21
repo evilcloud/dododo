7e1a1dc40dabe020f7dc3e1c3b291f831c8204c4
GK <evildatacloud@gmail.com>
2023-09-21 22:27:33 +0800

    moved createTask from Task to New

--- app/Commands/New.hs
+++ app/Commands/New.hs
@@ -3,12 +3,28 @@ module Commands.New
   )
 where
 
+import qualified Generator
 import qualified Task
 import qualified TasksIO
+import Timestamp
+
+-- Function to create a new task
+createTask :: String -> IO Task.Task
+createTask message = do
+  taskId <- Generator.taskID
+  timestamp <- Timestamp.createTimestamp
+  return
+    Task.Task
+      { Task.taskId = taskId,
+        Task.taskMessage = message,
+        Task.creationTimestamp = timestamp,
+        Task.status = "open",
+        Task.statusDuration = "none"
+      }
 
 -- Function for the 'new' command
 new :: String -> IO ()
 new message = do
-  task <- Task.createTask message
+  task <- createTask message
   TasksIO.addTaskToCurrent task
   putStrLn $ Task.formatTask task
--- app/Task.hs
+++ app/Task.hs
@@ -1,14 +1,12 @@
 module Task
   ( Task (..),
-    createTask,
     updateTask,
     formatTask,
   )
 where
 
 import qualified Config
-import qualified Generator
-import Timestamp
+import Timestamp (createTimestamp, timeDiffCasual)
 
 data Task = Task
   { taskId :: String,
@@ -19,24 +17,10 @@ data Task = Task
   }
   deriving (Show, Eq, Ord)
 
--- Function to create a new task
-createTask :: String -> IO Task
-createTask message = do
-  taskId <- Generator.taskID
-  timestamp <- createTimestamp
-  return
-    Task
-      { taskId = taskId,
-        taskMessage = message,
-        creationTimestamp = timestamp,
-        status = "open",
-        statusDuration = "none"
-      }
-
 -- Function to update a task's status
 updateTask :: Task -> String -> IO Task
 updateTask task newStatus = do
-  timestamp <- createTimestamp
+  timestamp <- Timestamp.createTimestamp
   let duration = Timestamp.timeDiffCasual (creationTimestamp task) timestamp
   return task {status = newStatus, statusDuration = duration}
 
@@ -58,5 +42,3 @@ formatTask task =
            then separator ++ status task ++ " " ++ statusDuration task
            else ""
        )
-
--- ++ "\n"
