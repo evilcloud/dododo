929b049a3b703544076f5329537cd96e6c19aed1
GK <evildatacloud@gmail.com>
2023-09-21 23:21:43 +0800

    sorting in descending order on every operation that overwrites the file

--- app/TasksIO.hs
+++ app/TasksIO.hs
@@ -14,12 +14,6 @@ import qualified FileManager
 import qualified Task
 import qualified TaskParser
 
--- Function to add a task to the current file
-addTaskToCurrent :: Task.Task -> IO ()
-addTaskToCurrent task = do
-  let formattedTask = Task.formatTask task ++ "\n"
-  FileManager.appendToFile Config.current formattedTask
-
 -- Function to get all tasks from the current file
 getAllTasksFromCurrent :: IO [Task.Task]
 getAllTasksFromCurrent = do
@@ -28,25 +22,36 @@ getAllTasksFromCurrent = do
   let maybeTasks = map TaskParser.parseTask contentLines
   return $ mapMaybe id maybeTasks
 
+-- Function to add a task to the current file
+addTaskToCurrent :: Task.Task -> IO ()
+addTaskToCurrent task = do
+  tasks <- getAllTasksFromCurrent
+  let updatedTasks = task : tasks
+  let sortedTasks = map Task.formatTask $ sortTasks updatedTasks
+  let content = unlines sortedTasks
+  FileManager.updateFile Config.current content
+
 -- Function to update a task in the current file
 updateTaskInCurrent :: Task.Task -> IO ()
 updateTaskInCurrent updatedTask = do
   tasks <- getAllTasksFromCurrent
   let updatedTasks = map (\task -> if Task.taskId task == Task.taskId updatedTask then updatedTask else task) tasks
-  let sortedTasks = sortOn Task.creationTimestamp updatedTasks
-  let formattedTasks = map Task.formatTask sortedTasks
-  let content = unlines formattedTasks
+  let sortedTasks = map Task.formatTask $ sortTasks updatedTasks
+  let content = unlines sortedTasks
   FileManager.updateFile Config.current content
 
 deleteTaskInCurrent :: String -> IO ()
 deleteTaskInCurrent taskIdToDelete = do
   tasks <- getAllTasksFromCurrent
   let updatedTasks = filter (\task -> Task.taskId task /= taskIdToDelete) tasks
-  let sortedTasks = sortOn Task.creationTimestamp updatedTasks
-  let formattedTasks = map Task.formatTask sortedTasks
-  let content = unlines formattedTasks
+  let sortedTasks = map Task.formatTask $ sortTasks updatedTasks
+  let content = unlines sortedTasks
   FileManager.updateFile Config.current content
 
 -- Function to find a task by ID
 findTaskById :: String -> [Task.Task] -> Maybe Task.Task
 findTaskById id = find ((== id) . Task.taskId)
+
+-- Function to sort tasks by creation timestamp
+sortTasks :: [Task.Task] -> [Task.Task]
+sortTasks = sortOn Task.creationTimestamp
